276697943a81325c5a43d8c3628d0daf5a7171a0
Laura McGann <hailey.mcgann@gmail.com>
2024-09-12 15:18:38 -0400

    ContactGroup HW interface

---
+++ src/EStimLibrary/Extensions/HardwareInterfaces/ContactGroup.cs
@@ -0,0 +1,19 @@
+﻿using EStimLibrary.Core.HardwareInterfaces;
+
+
+namespace EStimLibrary.Extensions.HardwareInterfaces;
+
+
+public class ContactGroup : NeuralInterfaceHardware
+{
+    public override string Name => "Contact Group";
+
+    public override int NumContacts => this._NumContacts;
+    protected int _NumContacts;
+
+    public ContactGroup(int numContacts) : base()
+    {
+        this._NumContacts = numContacts;
+    }
+}
+
