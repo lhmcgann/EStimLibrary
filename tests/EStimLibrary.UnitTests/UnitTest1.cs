cf6b8c1610dcd35128b29c834a53e12af2b6e521
Laura McGann <hailey.mcgann@gmail.com>
2024-09-12 16:12:10 -0400

    Output for test console write

--- tests/EStimLibrary.UnitTests/UnitTest1.cs
+++ tests/EStimLibrary.UnitTests/UnitTest1.cs
@@ -1,10 +1,22 @@
+using Xunit.Abstractions;
+
+
 namespace EStimLibrary.UnitTests;
+
 
 public class UnitTest1
 {
+    private readonly ITestOutputHelper _output;
+
+    public UnitTest1(ITestOutputHelper testOutputHelper)
+    {
+        this._output = testOutputHelper;
+    }
+
     [Fact]
     public void Test1()
     {
-
+        this._output.WriteLine("This should print.");
+        Assert.True(true);
     }
 }
