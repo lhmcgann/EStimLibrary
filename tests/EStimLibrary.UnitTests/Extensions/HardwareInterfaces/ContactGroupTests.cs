9714a618dc5308bdc2ab53fbe7a2e86bd8616cf5
Thao Nguyen <71063988+fernthao@users.noreply.github.com>
2024-11-18 15:01:31 -0500

    Tests for GelPad and ContactGroup (#35)
    * Tests for GelPad and ContactGroup
    
    * Add config to be able to run command dotnet test
    
    * Throw exception for non-positive NumContact
    
    * Add docstring and fix styling for ContactGroup.cs
    
    * Fix styling and directory structure for HardwareInterfacesTests
    
    * Revert "Add config to be able to run command dotnet test"
    
    This reverts commit 1c57537b616063f34e9510bf173edda3e1766222.
    
    * Added comments
    
    * Added Id check
    
    * Added comments on test method params
    
    ---------
    
    Co-authored-by: Laura McGann <hailey.mcgann@gmail.com>

---
+++ tests/EStimLibrary.UnitTests/Extensions/HardwareInterfaces/ContactGroupTests.cs
@@ -0,0 +1,46 @@
+using EStimLibrary.Extensions.HardwareInterfaces;
+
+
+namespace EStimLibrary.UnitTests.Extensions.HardwareInterfaces;
+
+
+/// <summary>
+/// A test class for the ContactGroup INeuralInterfaceHardware class.
+/// </summary>
+public class ContactGroupTests
+{
+    /// <summary>
+    /// Test the parameterized constructor with different valid parameters,
+    /// i.e., positive integer values for number of contacts. The neural
+    /// interface object should initialize with correct NumContact and Id
+    /// values.
+    /// </summary>
+    /// <param name="numContacts">The number of contacts to test ContactGroup
+    /// construction with.</param>
+    [Theory]
+    [InlineData(1)]     // Valid single contact.
+    [InlineData(10)]    // Valid multiple contacts.
+    public void Constructor_ShouldInit(int numContacts)
+    {
+        var contactGroup = new ContactGroup(numContacts);
+
+        Assert.Equal(-1, contactGroup.Id);
+        Assert.Equal(numContacts, contactGroup.NumContacts);
+    }
+
+    /// <summary>
+    /// Test the parameterized constructor with different invalid parameters,
+    /// i.e., 0 or negative integer values for number of contacts.
+    /// Constructioin should fail and throw an argument exception.
+    /// </summary>
+    /// <param name="numContacts">The number of contacts to test ContactGroup
+    /// construction with.</param>
+    [Theory]
+    [InlineData(0)]     // Invalid 0 contacts.
+    [InlineData(-1)]    // Invalid single negative contact number.
+    [InlineData(-10)]   // Invalid multiple negative contact number.
+    public void Constructor_ShouldThrowException(int numContacts)
+    {
+        Assert.Throws<ArgumentException>(() => new ContactGroup(numContacts));
+    }
+}
