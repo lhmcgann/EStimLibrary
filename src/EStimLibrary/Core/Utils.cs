e811a3ae69a1b6e279b1644ff127e253826526ad
bacchuongdaungo <hieuvtrung@gmail.com>
2025-02-06 04:42:52 -0500

    Deleted GetAvailableStimulatorTypes(), added new test cases and added doc comments for them, refactored temp interfaces to avoid confusion, uniformed test name style

--- src/EStimLibrary/Core/Utils.cs
+++ src/EStimLibrary/Core/Utils.cs
@@ -276,28 +276,6 @@ public static class Utils
         return false;
     }
 
-    // TODO: can delete once the above is tested for Stimulator
-    /// <summary>
-    /// Get all specific Stimulator derived class Types available in this
-    /// AppDomain.
-    /// </summary>
-    /// <returns>A dictionary of derived stimulator class (string name, Type)
-    /// pairs.</returns>
-    public static Dictionary<string, Type> GetAvailableStimulatorTypes()
-    {
-        // Get a list of all the derived classes (Types) of Stimulator.
-        List<Type> stimTypes = AppDomain.CurrentDomain.GetAssemblies()
-            .SelectMany(x => x.GetTypes())
-            .Where(x => typeof(Stimulator).IsAssignableFrom(x) &&
-                !x.IsInterface && !x.IsAbstract)
-            .ToList();
-        // Return a dictionary of string names of derived classes to the class
-        // Type.
-        return stimTypes
-            .Select(t => (t.Name, t))
-            .ToDictionary(tuple => tuple.Name, tuple => tuple.t);
-    }
-
     /// <summary>
     /// Check if a certain type is a manufacturable product, i.e., it has an
     /// IFactory associated with it within this assembly.
