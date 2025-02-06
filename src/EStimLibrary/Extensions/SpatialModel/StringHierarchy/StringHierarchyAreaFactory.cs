52fb7dd7a957b8baebf888de051574e768438137
cld99 <115433496+cld99@users.noreply.github.com>
2025-02-05 20:40:56 -0500

    Add comments to factory classes and revise test cases.

--- src/EStimLibrary/Extensions/SpatialModel/StringHierarchy/StringHierarchyAreaFactory.cs
+++ src/EStimLibrary/Extensions/SpatialModel/StringHierarchy/StringHierarchyAreaFactory.cs
@@ -7,22 +7,40 @@ using EStimLibrary.Core.SpatialModel;
 namespace EStimLibrary.Extensions.SpatialModel.StringHierarchy;
 
 
-//public class StringHierarchySpecFactory<S> : IFactory<S>
-//    where S : StringHierarchySpec
+/// <summary>
+/// StringHierarchAreaFactories are objects used to create instances of the
+/// StringHierarchyArea class.
+/// </summary>
 public class StringHierarchyAreaFactory : IFactory<IArea>
 {
+    /// <summary>
+    /// The base region to create StringHierarchyAreas from.
+    /// </summary>
     private readonly StringHierarchyRegion _baseRegion;
 
+    /// <summary>
+    /// The help message describing the dynamic data validation performed.
+    /// </summary>
     public string HelpMsg { get; init; }
+
+    /// <summary>
+    /// The dictionary containing data validation objects for parameters
+    /// passed into the factory create method.
+    /// </summary>
     public Dictionary<string, IDataLimits> ParamLimits { get; init; }
 
+    /// <summary>
+    /// The constructor, which creates an instance of the factory with the
+    /// supplied base region and initializes validation.
+    /// </summary>
+    /// <param name="baseModelRegion">The base region to use.</param>
     public StringHierarchyAreaFactory(StringHierarchyRegion baseModelRegion)
     {
         this._baseRegion = baseModelRegion;
 
         this.HelpMsg = $"A StringHierarchyArea can be built from one of the " +
             $"following path specs, selecting one option from any list in [] " +
-            $"and excluding the []:\n{baseModelRegion?.ToString() ?? "null"}";
+            $"and excluding the []:\n{baseModelRegion.ToString()}";
 
         this.ParamLimits = new() {
             {"fullSpec",
@@ -31,12 +49,20 @@ public class StringHierarchyAreaFactory : IFactory<IArea>
 
     }
 
+    /// <summary>
+    /// The validation function used by the factory create method to ensure
+    /// the supplied area spec is valid for the provided base region. The
+    /// fullspec must provide a single region spec which is a subregion
+    /// of the base region, including at most one set of modifiers.
+    /// </summary>
+    /// <param name="fullSpec">The area spec to validate.</param>
+    /// <returns></returns>
     private bool AreaSpecCheckFunction(string fullSpec)
     {
         var parts = fullSpec.Split(
             StringHierarchySpec.REGIONS_MODIFIERS_DELIMITER);
 
-        return parts.Length < 3 &&
+        return parts.Length > 0 && parts.Length < 3 &&
             // First try to navigate this model to the specified region.
             this._baseRegion.TryGetSubregion(parts[0], out var subregion) &&
             // Then - if any given - check if the modifiers valid in the model.
@@ -44,6 +70,17 @@ public class StringHierarchyAreaFactory : IFactory<IArea>
             subregion.IsValidModifierSpec(parts[1]) : true);
     }
 
+    /// <summary>
+    /// The factory create method used to create StringHierarchyArea instances.
+    /// </summary>
+    /// <param name="paramValues">The parameters to pass into the
+    /// StringHierarchyArea constructor. This must include a valid area spec
+    /// with key "fullspec".</param>
+    /// <param name="product">An output parameter: the StringHierarchyArea if
+    /// created, else null.</param>
+    /// <param name="skipValueValidation">Whether to skip validation with
+    /// AreaSpecCheckFunction.</param>
+    /// <returns></returns>
     public bool TryCreate(Dictionary<string, object> paramValues,
         out IArea product, bool skipValueValidation = false)
     {
--- src/EStimLibrary/Extensions/SpatialModel/StringHierarchy/StringHierarchyLocationFactory.cs
+++ src/EStimLibrary/Extensions/SpatialModel/StringHierarchy/StringHierarchyLocationFactory.cs
@@ -7,16 +7,34 @@ using EStimLibrary.Core.SpatialModel;
 namespace EStimLibrary.Extensions.SpatialModel.StringHierarchy;
 
 
-//public class StringHierarchySpecFactory<S> : IFactory<S>
-//    where S : StringHierarchySpec
+/// <summary>
+/// StringHierarchyLocationFactories are objects used to create instances of
+/// the StringHierarchyLocation class.
+/// </summary>
 public class StringHierarchyLocationFactory :
     IFactory<ILocation>
 {
+    /// <summary>
+    /// The base region to create StringHierarchyLocations from.
+    /// </summary>
     private readonly StringHierarchyRegion _baseRegion;
 
+    /// <summary>
+    /// The help message describing the dynamic data validation performed.
+    /// </summary>
     public string HelpMsg { get; init; }
+
+    /// <summary>
+    /// The dictionary containing data validation objects for parameters
+    /// passed into the factory create method.
+    /// </summary>
     public Dictionary<string, IDataLimits> ParamLimits { get; init; }
 
+    /// <summary>
+    /// The constructor, which creates an instance of the factory with the
+    /// supplied base region and initializes validation.
+    /// </summary>
+    /// <param name="baseModelRegion">The base region to use.</param>
     public StringHierarchyLocationFactory(
         StringHierarchyRegion baseModelRegion)
     {
@@ -24,8 +42,7 @@ public class StringHierarchyLocationFactory :
 
         this.HelpMsg = $"A StringHierarchyLocation can be built from one of " +
             $"the following path specs, selecting one option from any list " +
-            $"in [] and excluding the []:\n{baseModelRegion?.ToString() ??
-            "null"}";
+            $"in [] and excluding the []:\n{baseModelRegion.ToString()}";
 
         this.ParamLimits = new() {
             {"fullSpec",
@@ -34,12 +51,20 @@ public class StringHierarchyLocationFactory :
 
     }
 
+    /// <summary>
+    /// The validation function used by the factory create method to ensure
+    /// the supplied location spec is valid for the provided base region.
+    /// The fullspec must provide a single region spec which is a subregion
+    /// of the base region, including at most one set of modifiers.
+    /// </summary>
+    /// <param name="fullSpec">The location spec to validate.</param>
+    /// <returns></returns>
     private bool LocationSpecCheckFunction(string fullSpec)
     {
         var parts = fullSpec.Split(
             StringHierarchySpec.REGIONS_MODIFIERS_DELIMITER);
 
-        return parts.Length < 3 &&
+        return parts.Length > 0 && parts.Length < 3 &&
             // First try to navigate this model to the specified region.
             this._baseRegion.TryGetSubregion(parts[0], out var subregion) &&
             // Then - if any given - check if the modifiers valid in the model.
@@ -47,6 +72,18 @@ public class StringHierarchyLocationFactory :
             subregion.IsValidModifierSpec(parts[1]) : true);
     }
 
+    /// <summary>
+    /// The factory create method used to create StringHierarchyLocation
+    /// instances. 
+    /// </summary>
+    /// <param name="paramValues">The parameters to pass into the
+    /// StringHierarchyLocation constructor. This must include a valid location
+    /// spec with key "fullspec".</param>
+    /// <param name="product">An output parameter: the StringHierarchyLocation
+    /// if created, else null.</param>
+    /// <param name="skipValueValidation">Whether to skip validation with
+    /// LocationSpecCheckFunction.</param>
+    /// <returns></returns>
     public bool TryCreate(Dictionary<string, object> paramValues,
         out ILocation product, bool skipValueValidation = false)
     {
