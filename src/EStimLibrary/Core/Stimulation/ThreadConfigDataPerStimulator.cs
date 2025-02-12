3c00491283f2bc5877beb547a899ebeb084402fd
Laura McGann <hailey.mcgann@gmail.com>
2025-02-12 15:55:21 -0500

    ThreadConfig: rename param to match renaming in Stimulator

--- src/EStimLibrary/Core/Stimulation/ThreadConfigDataPerStimulator.cs
+++ src/EStimLibrary/Core/Stimulation/ThreadConfigDataPerStimulator.cs
@@ -7,7 +7,7 @@ namespace EStimLibrary.Core.Stimulation;
 
 public record ThreadConfigDataPerStimulator(int GlobalStimId,
     IEnumerable<Lead> IndependentLeads,
-    Dictionary<string, Tuple<IDataLimits, object>> StimParamData,
+    Dictionary<string, Tuple<IDataLimits, object>> StimParamSpecs,
     // TODO: edit once have sorted how will resolve StimParams and enum stuff.
     SortedSet<string> ModulatableStimParams);
 
